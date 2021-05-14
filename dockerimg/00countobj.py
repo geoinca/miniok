19945359bff115821392e7318577fac40d7e69d2
geo.inca <geo.inca@hotmail.com>
2021-05-14 14:46:30 +0200

    webhook minio folder

--- dockerimg/00countobj.py
+++ dockerimg/00countobj.py
@@ -12,12 +12,18 @@ def check(s3AccessKey,s3SecretAccessKey,s3EndPointUrl,s3Bucket,s3BucketOut,s3Out
                         region_name='us-east-1')
 
     my_bucket = s3.Bucket(s3Bucket)
-    countObj=0
+    result=0
     for object_summary in my_bucket.objects.filter(Prefix=s3Prefix):
-        countObj+=1
-    
-    print (countObj)
+        result+=1
 
+    print (result)
+    if result>2:
+        result=3
+    a = ["doNothing", "faceAlignment", "warpImages", "panorama"]
+    f = open("/tmp/tfmresult.txt", "w")
+
+    f.write(a[result])
+    f.close()
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
@@ -34,4 +40,11 @@ if __name__ == '__main__':
         print (0)
 
 
+s3AccessKey = 'tfq0M5o1QtNOJcP1nizr'
+s3SecretKey = 'HbO5COQOXR6z3P0jgTVCBzWxkXFPXKsMqoItRzL6'
+s3EndPointUrl = 'http://argo-artifacts:9000'
+s3Bucket='infolder'
+s3BucketOut='outfolder'
+s3OutFileName="imgresult0000.jpeg"
+s3Prefix="id1/"
  
